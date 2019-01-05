1ba42ebff2fa740823f28deee85663bc435bd939
Nao Okada <naookada@Naos-MacBook-Pro.local>
2019-01-05 12:24:41 +0900

    Changing through table name

--- app/models/channel.rb
+++ app/models/channel.rb
@@ -1,5 +1,5 @@
 class Channel < ApplicationRecord
     has_many :messages
-    has_many :users, through: :groups_users
-    has_many :groups_users
+    has_many :users, through: :channels_users
+    has_many :channels_users
 end
