64cdeff71eb1282ca95fcd1aaa0e5c7b926aa126
Nao Okada <naookada@Naos-MacBook-Pro.local>
2019-01-05 12:21:05 +0900

    Set association between user and message models

--- app/models/channel.rb
+++ app/models/channel.rb
@@ -1,3 +1,5 @@
 class Channel < ApplicationRecord
     has_many :messages
+    has_many :users, through: :groups_users
+    has_many :groups_users
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,6 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
   validates :name, presence: true
   has_many :messages
+  has_many :users, through: :groups_users
+  has_many :groups_users
 end
