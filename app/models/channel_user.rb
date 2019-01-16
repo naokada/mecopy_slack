7ea893355c1564412c83d95361eccb8a5f5aa426
Nao Okada <naookada@Naos-MacBook-Pro.local>
2019-01-16 14:12:58 +0900

    Define association for feed content

--- app/models/channel_user.rb
+++ app/models/channel_user.rb
@@ -1,4 +1,5 @@
 class ChannelUser < ApplicationRecord
   belongs_to :user
   belongs_to :channel
+  has_one :feed_content, as: :content, dependent: :destroy
 end
--- app/models/message.rb
+++ app/models/message.rb
@@ -3,5 +3,5 @@ class Message < ApplicationRecord
 	belongs_to :user
 	validates :content, presence: true
 	after_create_commit {MessageBroadcastJob.perform_later self}
-
+	has_one :feed_content, as: :content, dependent: :destroy
 end
