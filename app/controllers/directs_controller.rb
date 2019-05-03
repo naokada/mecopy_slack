76ecc9e614732458f6649ef9ba795a2741e98d9d
Nao Okada <naookada@Naos-MacBook-Pro-2.local>
2019-05-03 19:46:02 +0900

    Make method to find duplicated direct message id

--- app/controllers/directs_controller.rb
+++ app/controllers/directs_controller.rb
@@ -14,38 +14,21 @@ class DirectsController < ApplicationController
 
   def create
     # TODO 誰も選択していない時の処理
-
-    @direct = Direct.new
     user_ids = direct_params[:user_ids].map(&:to_i)
-    user_ids << current_user.id
-    user_ids = user_ids.sort
-    user_directs = current_user.directs
-
-    existed_direct_users = []
-    user_directs.each do |user_direct|
-      existed_direct_user = {}
-      existed_direct_user[:id] = user_direct.id
-      existed_direct_user[:users] = user_direct.user_ids
-      existed_direct_users << existed_direct_user
-    end
-
-    duplicated_dm = existed_direct_users.select {|edu| edu[:users] == user_ids}
-
-    respond_to do |format|
-      if duplicated_dm.length == 0
-        if user_ids.count > 0 && @direct.save
-          user_ids.each do |user_id|
-            DirectUser.create(direct_id: @direct.id, user_id:user_id)
-          end
-          format.html { redirect_to @direct, notice: 'DM room was successfully created.' }
-          format.json { render :show, status: :created, location: @direct }
-        else
-          format.html { render :new }
-          format.json { render json: @direct.errors, status: :unprocessable_entity }
+    id = Direct.get_duplicated_dm_id(current_user, user_ids)
+    if id == -1
+      @direct = Direct.create
+      DirectUser.transaction do
+        user_ids.each do |user_id|
+          DirectUser.create(direct_id: @direct.id, user_id:user_id)
         end
-      else
-        format.html { redirect_to Direct.find(duplicated_dm[0][:id])}
       end
+      respond_to do |format|
+        format.html { redirect_to @direct, notice: 'DM room was successfully created.' }
+        format.json { render :show, status: :created, location: @direct }
+      end
+    else
+      redirect_to Direct.find(id)
     end
   end
 
--- app/models/direct.rb
+++ app/models/direct.rb
@@ -5,6 +5,25 @@ class Direct < ApplicationRecord
 
   # before_validation: :is_unique
 
-  # def is_unique
-  # end
+  # Return an id of duplicated direct message room when
+  # a given user has a direct message room with given users
+  # Otherwise, it returns -1
+  # It takes an instance of User model and integer arrays of users' id
+  def self.get_duplicated_dm_id(user, user_ids)
+    user_ids << user.id
+    user_ids = user_ids.sort
+    user_directs = user.directs
+
+    existed_directs = []
+    user_directs.each do |user_direct|
+      existed_directs << {id: user_direct.id, user_ids: user_direct.user_ids.sort}
+    end
+
+    duplicated_dm = existed_directs.select {|existed_direct| existed_direct[:user_ids] == user_ids}
+    if duplicated_dm.length > 0
+      return duplicated_dm[0][:id]
+    else
+      return -1
+    end
+  end
 end
